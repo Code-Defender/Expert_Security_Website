0cbae2fa1c4c75359594af69f43360e4a4e6db4d
Code-Defender <121310353+Code-Defender@users.noreply.github.com>
2026-05-23 23:15:26 +0500

    chore: update Google Apps Script Web App URL to the deployed endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const CONFIG = {
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/18ZQvS9HcZs8oCXb9L7lRed7S4_9GRM_VuUkTgxx2i8KPg1U9Zr340Pbq/exec"
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyhliO9T01gJlfVfK8esehAkt5UV3kfqSfZwlVcSHwcZ5iSkT631-hFzUW7sLzb_uXT/exec"
 };
